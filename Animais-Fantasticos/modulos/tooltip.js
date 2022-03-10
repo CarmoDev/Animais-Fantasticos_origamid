18af17664c41465b527023a817bbf5192a86e619
Jefferson Santos <dalvatechnologies@gmail.com>
2022-03-10 19:13:13 -0300

    refatoração git(aula 11-1)
    Na aula 1 do módulo 11 da origamid, refatoramos, lançamos tudo que fizemos no módulo 10 para o git

---
+++ Animais-Fantasticos/modulos/tooltip.js
@@ -0,0 +1,45 @@
+export default function initTooltip() {
+  const tooltips = document.querySelectorAll("[data-tooltip]");
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = `${event.pageY + 20}px`;
+      this.tooltipBox.style.left = `${event.pageX + 20}px`;
+      this.element.removeEventListener("mousemove", onMouseMove);
+    },
+  };
+
+  const onMouseLeave = {
+    // essa é uma forma de usar elementos q não tem estão nesse escopo
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener("mouseleave", onMouseLeave);
+    },
+  };
+
+  function createTooltipBox(item) {
+    const tooltipBox = document.createElement("div");
+    const text = item.getAttribute("aria-label");
+    tooltipBox.classList.add("tooltip");
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+
+  function onMouseOver(event) {
+    const tooltipBox = createTooltipBox(this);
+    tooltipBox.style.top = `${event.pageY}px`;
+    tooltipBox.style.left = `${event.pageX}px`;
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener("mousemove", onMouseMove);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener("mouseleave", onMouseLeave);
+  }
+
+  tooltips.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  });
+}
---
+++ Animais-Fantasticos/script.js
@@ -0,0 +1,36 @@
+import initEasyScroll from "./modulos/easyScroll.js";
+import initAnimateScroll from "./modulos/scroll-animation.js";
+import initAccordion from "./modulos/accordion.js";
+import initTabNav from "./modulos/initTab-nav.js";
+import initBackground from "./modulos/background.js";
+import initModal from "./modulos/modal.js";
+import initTooltip from "./modulos/tooltip.js";
+import initDropDownMenu from "./modulos/dropdown-menu.js";
+import initMenuMobile from "./modulos/menu-mobile.js";
+import initDateObject from "./modulos/date-object.js";
+import initFetchAnimais from "./modulos/fetchAnimais.js";
+import initFetchBitcoin from "./modulos/fetch-bitcoin.js";
+
+initEasyScroll();
+
+initAnimateScroll();
+
+initAccordion();
+
+initTabNav();
+
+initBackground();
+
+initModal();
+
+initTooltip();
+
+initDropDownMenu();
+
+initMenuMobile();
+
+initDateObject();
+
+initFetchAnimais();
+
+initFetchBitcoin();
