2a6d8832abf923905969a5a8bf06afbd7b98b7f8
Jefferson Santos <dalvatechnologies@gmail.com>
2022-03-13 07:43:47 -0300

    EasyScroll transformado em classe

--- modulos/easyScroll.js
+++ modulos/easyScroll.js
@@ -1,21 +1,32 @@
-export default function initEasyScroll() {
-  const linksInternos = document.querySelectorAll(
-    '[data-anime="menu"] a[href^="#"]',
-  );
+export default class EasyScroll {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
 
-  function scrollToSection(Event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(Event) {
     Event.preventDefault();
     const href = Event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
-  console.log('teste');
+  init() {
+    if(this.link)
+      this.addLinkEvent();
+      return this;
+  }
 }
